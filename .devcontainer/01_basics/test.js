431709dc94aebc87011728f072751510b58c3cbc
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2023-12-15 08:39:05 +0000

    variable and datatype

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,8 @@
-console.log("ganaesh")
+const accountId = 123445;
+let  accountEmail = 'Ganesh@goole.com';
+var  accountName = "Ganesh"
+accountCity="Lucknow"
+console.log(accountEmail);
+console.log(accountId);
+console.table([accountId,accountName,accountCity,accountEmail]);
+console.log(typeof(undefined))
