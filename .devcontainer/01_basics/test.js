0d65881d1657a959257901b529c952e63e01a8e9
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2023-12-15 08:47:32 +0000

    datatype and variable with explanation

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,8 +1,13 @@
-const accountId = 123445;
-let  accountEmail = 'Ganesh@goole.com';
-var  accountName = "Ganesh"
-accountCity="Lucknow"
-console.log(accountEmail);
+const accountId = 123445;//its value naver be change
+let accountEmail = 'Ganesh@goole.com';//general type of declare a variable
+
+var accountName = "Ganesh" //not identify scope in the code so it is not good to declare a variable
+
+accountCity = "Lucknow"//not good to declare variable 
+
+console.log(accountEmail);//print only one line on the screen
 console.log(accountId);
-console.table([accountId,accountName,accountCity,accountEmail]);
-console.log(typeof(undefined))
+console.table([accountId, accountName, accountCity, accountEmail]);//print more then one data in tabular form
+console.log(typeof (undefined))//undefined
+console.log(typeof ("Ganesh"))//string
+console.log(typeof (null))//type of null is OBJECT 
